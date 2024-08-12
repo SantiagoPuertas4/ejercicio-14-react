b3c2eb1ac279f9bbbf93692152726712ae65cded
Santiago Puertas <santidoors13@gmail.com>
2024-08-12 17:57:24 -0300

    Agrego entradas a home

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,4 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBlogsFn } from "../api/blogs";
+import BlogCard from "../components/Home/BlogCard";
+
 const HomeView = () => {
-  return <div>HomeView</div>;
+  const {
+    data: blogs,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: getBlogsFn,
+  });
+
+  if (isLoading) {
+    return <p className="mt-3 text-center">Cargando entrada ...</p>;
+  }
+
+  if (isError) {
+    return (
+      <div className="alert alert-danger">
+        Ocurrio un error leyendo las entradas
+      </div>
+    );
+  }
+
+  if (blogs && blogs.length === 0) {
+    return (
+      <div className="alert alert-info">
+        No se encontraron entradas de blog para mostrar
+      </div>
+    );
+  }
+
+  return (
+    <section className="row">
+      {blogs.map((blog) => {
+        return (
+          <article className="col-12 col-md-4 col-lg-3" key={blog.id}>
+            <BlogCard blog={blog} />
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 export default HomeView;
