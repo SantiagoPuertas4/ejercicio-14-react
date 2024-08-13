2421eab320ff6a310f802f26e195984b6962b3bb
Santiago Puertas <santidoors13@gmail.com>
2024-08-13 14:39:13 -0300

    Agrego edicion de entradas

--- src/api/blogs.js
+++ src/api/blogs.js
@@ -35,3 +35,17 @@ export const deleteBlogFn = async (blogId) => {
     );
   }
 };
+
+export const putBlogFn = async ({ blogId, data }) => {
+  const res = await fetch(`${BACKEND_URL}/blogs/${blogId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error("Ocurrio un error intentando editar el blog seleccionado");
+  }
+};
